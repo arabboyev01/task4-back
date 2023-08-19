d4d4689ab6cb8ab7ead4417184aad4276bc3d696
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-08-19 15:04:03 +0500

    argon2

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,7 +1,44 @@
+// import express from 'express';
+// import db from "../knexfile";
+// import jwt from "jsonwebtoken";
+// import bcrypt from "bcrypt";
+
+// const loginRoute = express.Router();
+
+// loginRoute.post('/', async (req, res) => {
+//     const { username, password } = req.body;
+
+//     try {
+//         const user = await db('users').where('username', username).first();
+//         const getStatus = await db('users').where('username', username).select('status').first();
+
+//         if (!user) {
+//             return res.status(401).json({ error: "Invalid credentials" });
+//         }
+
+//         const passwordMatch = await bcrypt.compare(password, user.password);
+//         if (getStatus.status === "blocked") {
+//             return res.status(403).json({ error: "You are blocked account" });
+//         }
+
+//         if (!passwordMatch) {
+//             return res.status(401).json({ error: "Invalid credentials" });
+//         }
+//         if (passwordMatch && getStatus.status === "active") {
+//             const token = jwt.sign({ username }, 'secret');
+//             res.json({ token });
+//         }
+
+//     } catch (error) {
+//         res.status(500).json({ error: "An error occurred during login." });
+//     }
+// });
+
+// export { loginRoute };
 import express from 'express';
 import db from "../knexfile";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import argon2 from "argon2";
 
 const loginRoute = express.Router();
 
@@ -16,14 +53,16 @@ loginRoute.post('/', async (req, res) => {
             return res.status(401).json({ error: "Invalid credentials" });
         }
 
-        const passwordMatch = await bcrypt.compare(password, user.password);
+        const passwordMatch = await argon2.verify(user.password, password);
+
         if (getStatus.status === "blocked") {
-            return res.status(403).json({ error: "You are blocked account" });
+            return res.status(403).json({ error: "You are a blocked account" });
         }
 
         if (!passwordMatch) {
             return res.status(401).json({ error: "Invalid credentials" });
         }
+
         if (passwordMatch && getStatus.status === "active") {
             const token = jwt.sign({ username }, 'secret');
             res.json({ token });
--- src/routes/signUp.ts
+++ src/routes/signUp.ts
@@ -1,7 +1,32 @@
+// import express from 'express';
+// import db from "../knexfile";
+// import jwt from "jsonwebtoken";
+// import bcrypt from "bcrypt";
+
+// const signUpRoute = express.Router();
+
+// signUpRoute.post('/', async (req, res) => {
+//     const { firstname, lastname, username, password } = req.body;
+
+//     try {
+//         const hashedPassword = await bcrypt.hash(password, 10);
+
+//         await db('users').insert({ firstname, lastname, username, password: hashedPassword });
+
+//         const token = jwt.sign({ username }, 'secret');
+
+//         res.send({ token });
+//     } catch (error) {
+//         res.status(500).send({ error: "An error occurred during sign-up." });
+//     }
+// });
+
+// export { signUpRoute };
+
 import express from 'express';
 import db from "../knexfile";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import argon2 from "argon2";
 
 const signUpRoute = express.Router();
 
@@ -9,7 +34,7 @@ signUpRoute.post('/', async (req, res) => {
     const { firstname, lastname, username, password } = req.body;
 
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await argon2.hash(password);
 
         await db('users').insert({ firstname, lastname, username, password: hashedPassword });
 
